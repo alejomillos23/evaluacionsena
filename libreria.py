6444135886ca519892d72523f9ab72d88350c999
kevintorres1826 <kesantitorres@gmail.com>
2026-03-13 07:40:55 -0500

    se corrige error de indentacion en el codigo

--- libreria.py
+++ libreria.py
@@ -7,7 +7,7 @@ nombre_usuario = input("Ingrese el nombre del usuario: ")
 nombre_libro = input("Nombre del libro que desea prestar: ")
 print("\nVerificacion de datos del usuario")
 tiene_carnet = input("¿El usuario tiene carnet? (si/no): ")
- tiene_multas = input("¿El usuario tiene multas pendientes? (si/no): ")
+tiene_multas = input("¿El usuario tiene multas pendientes? (si/no): ")
 
 print("\nProcesando solicitud de prestamo...")
 
