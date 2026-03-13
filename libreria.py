828d233b65aed614f9313ead4f18b8b3f27fb764
kevintorres1826 <kesantitorres@gmail.com>
2026-03-13 07:36:49 -0500

    se agrega logica del prestamo y mensajes finales

--- libreria.py
+++ libreria.py
@@ -13,3 +13,18 @@ print("\nProcesando solicitud de prestamo...")
 
 dias_prestamo = 7
 fecha_prestamo = "13/03/2026"
+
+if tiene_carnet == "si" and tiene_multas == "no":
+
+    print("Prestamo aprobado")
+    print("Usuario:", nombre_usuario)
+    print("Libro:", nombre_libro)
+    print("Dias de prestamo:", dias_prestamo)
+    print("Fecha del prestamo:", fecha_prestamo)
+
+else:
+
+    print("No se puede prestar el libro")
+
+print("\nGracias por usar el sistema de biblioteca")
+print("Fin del programa")
